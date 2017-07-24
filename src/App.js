9d61ce44536b508e3cf24b1923d8d6ca3cd1ec9f
Kathleen French <kfrench09@gmail.com>
2017-07-24 15:06:29 -0400

    connect tag from to api and correct tag bug on new note creation

--- src/App.js
+++ src/App.js
@@ -61,6 +61,16 @@ class App extends Component {
     this.setState({ newTag: true })
   }
 
+  closeTagForm = () => {
+    this.setState({ newTag: false })
+  }
+
+  submitTag = (data, noteId) => {
+    axios.post(urlFor(`notes/${noteId}/tags`), data)
+    .then((res) => this.getNote(noteId))
+    .catch((err) => console.log(err.response.data))
+  }
+
   render() {
     const { showNote, notes, note, newTag } = this.state
 
@@ -73,6 +83,8 @@ class App extends Component {
             submitNote={this.submitNote}
             showTagForm={this.showTagForm}
             newTag={newTag}
+            closeTagForm={this.closeTagForm}
+            submitTag={this.submitTag}
           /> 
           : 
           <List 
