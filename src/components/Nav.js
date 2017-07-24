b1bd1d9900578117ff2cfadd836ff34f97f58b62
Kathleen French <kfrench09@gmail.com>
2017-07-24 17:12:26 -0400

    update titles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ class Nav extends React.Component {
 
         return (
             <div className="nav-container">
-                <div className="nav-logo">Anon Dump</div>
+                <div className="nav-logo">Note Taker</div>
                 <div className="nav-button" onClick={ () => toggleNote()} >
                     { showNote ? <span><i className="add-note material-icons">clear</i></span> : <span><i className="add-note material-icons">add</i></span> }
                 </div>
